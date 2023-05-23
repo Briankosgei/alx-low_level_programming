5abe0008c8cda85a7d600447d137eeb31cc00a44
Briankosgei <kipkosgeib42@gmail.com>
2023-05-23 04:16:36 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,23 +2,11 @@
 
 
 /**
-* main - check the code.
-*
-* Return: Always 0.
+* _isalpha - checks for alphabetic character
+* @c: the character to be checked
+* Return: 1 if c is a letter, 0 otherwise
 */
-int main(void)
+int _isalpha(int c)
 {
-int r;
-
-
-r = _isalpha('H');
-_putchar(r + '0');
-r = _isalpha('o');
-_putchar(r + '0');
-r = _isalpha(108);
-_putchar(r + '0');
-r = _isalpha(';');
-_putchar(r + '0');
-_putchar('\n');
-return (0);
+return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
