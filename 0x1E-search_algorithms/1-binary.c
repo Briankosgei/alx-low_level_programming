d11c0d0755f3b71a9dbf61263ee4c13f6686fa91
kipkosgei <kipkosgei@example.com>
2026-01-06 10:20:57 +0300

    all

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,39 +1,73 @@
+#include <stdio.h>
 #include "search_algos.h"
 
 /**
-  * binary_search - Searches for a value in a sorted array
-  *                 of integers using binary search.
-  * @array: A pointer to the first element of the array to search.
-  * @size: The number of elements in the array.
-  * @value: The value to search for.
-  *
-  * Return: If the value is not present or the array is NULL, -1.
-  *         Otherwise, the index where the value is located.
-  *
-  * Description: Prints the [sub]array being searched after each change.
-  */
+ * print_array - prints an array of integers
+ * @array: pointer to the first element of the array
+ * @left: starting index to print
+ * @right: ending index to print
+ */
+void print_array(int *array, size_t left, size_t right)
+{
+    size_t i;
+
+    printf("Searching in array: ");
+    for (i = left; i <= right; i++)
+    {
+        printf("%d", array[i]);
+        if (i < right)
+            printf(", ");
+    }
+    printf("\n");
+}
+
+/**
+ * binary_search - searches for a value in a sorted array
+ *                 using the Binary search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ * 
+ * Return: index where value is located, or -1 if not found
+ */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t i, left, right;
+    size_t left, right, mid;
 
-	if (array == NULL)
-		return (-1);
+    /* Check if array is NULL or empty */
+    if (array == NULL || size == 0)
+        return (-1);
 
-	for (left = 0, right = size - 1; right >= left;)
-	{
-		printf("Searching in array: ");
-		for (i = left; i < right; i++)
-			printf("%d, ", array[i]);
-		printf("%d\n", array[i]);
+    left = 0;
+    right = size - 1;
 
-		i = left + (right - left) / 2;
-		if (array[i] == value)
-			return (i);
-		if (array[i] > value)
-			right = i - 1;
-		else
-			left = i + 1;
-	}
+    /* Print the initial array */
+    print_array(array, left, right);
 
-	return (-1);
+    while (left <= right)
+    {
+        mid = left + (right - left) / 2;
+
+        /* Check if value is present at mid */
+        if (array[mid] == value)
+            return (mid);
+
+        /* If value is greater, ignore left half */
+        if (array[mid] < value)
+        {
+            left = mid + 1;
+            if (left <= right)
+                print_array(array, left, right);
+        }
+        /* If value is smaller, ignore right half */
+        else
+        {
+            right = mid - 1;
+            if (left <= right)
+                print_array(array, left, right);
+        }
+    }
+
+    /* Value not present in array */
+    return (-1);
 }
